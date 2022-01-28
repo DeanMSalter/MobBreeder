ceee2385a7058dcdb28bcf93994dea0db591bbd9
DeanMSalter <Dean.M.Salter@gmail.com>
2022-01-28 07:00:34 +0000

    basic breeding

--- src/McEssence/MobBreeder/Listeners.java
+++ src/McEssence/MobBreeder/Listeners.java
@@ -10,6 +10,8 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
+import java.util.List;
+
 public class Listeners implements Listener {
     @EventHandler(priority= EventPriority.NORMAL, ignoreCancelled=true)
     public void onRightClick(PlayerInteractEntityEvent event) {
@@ -18,6 +20,13 @@ public class Listeners implements Listener {
         if (event.getRightClicked().getType() == EntityType.DOLPHIN) {
             if (player.getInventory().getItemInMainHand().getType() == Material.COD) {
                 player.spawnParticle(Particle.HEART, entity.getLocation(), 5, 0, 1, 0);
+                List<Entity> nearbyEntities = entity.getNearbyEntities(5, 5 ,5);
+                for (Entity nearbyEntity : nearbyEntities) {
+                    if (nearbyEntity.getType() == EntityType.DOLPHIN) {
+                        player.getWorld().spawnEntity(entity.getLocation(), EntityType.DOLPHIN);
+                        break;
+                    }
+                }
             }
             player.sendMessage("right clicked dolphin");
         }
--- src/McEssence/MobBreeder/Main.java
+++ src/McEssence/MobBreeder/Main.java
@@ -4,11 +4,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.comphenix.protocol.ProtocolManager;
-import org.bukkit.Bukkit;
-import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.event.Listener;
-import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
